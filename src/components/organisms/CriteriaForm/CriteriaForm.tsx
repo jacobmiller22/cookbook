bf5468e581a4570ce012f6adc6c3eb2d00accfa5
jacobmiller22 <jacobmiller22@vt.edu>
2021-06-26 12:57:57 -0400

    Basic ingredient form. Not finished

--- src/components/organisms/CriteriaForm/CriteriaForm.tsx
+++ src/components/organisms/CriteriaForm/CriteriaForm.tsx
@@ -1,4 +1,240 @@
-const CriteriaForm = () => {
-  return <div>Criteria Form</div>;
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { Button, Typography, Grid, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+interface IFormInputs {
+  ingredient: string;
+  amount: string;
+  unit: string;
+  grade: string;
+}
+
+const CriteriaForm = ({ onSubmit }) => {
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "ingredients",
+  });
+  const classes = useStyles();
+
+  const renderError = (options, field) => {
+    if (!options) return null;
+    let text = "";
+    switch (options.type) {
+      case "pattern":
+        text = "";
+        break;
+      default:
+        text = "";
+        break;
+    }
+    return <p>{text}</p>;
+  };
+
+  const isOptionalRequired = (amount, unit) => {
+    return (amount != null && amount !== "") || (unit != null && unit !== "");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {fields.map(({ id, ingredient, amount, unit }, i) => {
+        return (
+          <Grid container spacing={1} key={id}>
+            <Grid item xs={6}>
+              <Controller
+                name={`ingredients[${i}].ingredient`}
+                control={control}
+                defaultValue={ingredient}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    placeholder="Ingredient"
+                    label="Ingredient*"
+                    variant="outlined"
+                    size="medium"
+                    helperText={renderError(
+                      errors?.ingredients?.[i]?.ingredient,
+                      {
+                        ingredient: getValues(`ingredients[${i}].ingredient`),
+                      }
+                    )}
+                    error={
+                      !!renderError(errors?.ingredients?.[i]?.ingredient, {
+                        ingredient: getValues(`ingredients[${i}].ingredient`),
+                      })
+                    }
+                    fullWidth
+                    {...field}
+                    onChange={(e) =>
+                      setValue(`ingredients[${i}].ingredient`, e.target.value, {
+                        shouldValidate: true,
+                      })
+                    }
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <Controller
+                name={`ingredients[${i}].amount`}
+                control={control}
+                defaultValue={amount}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    label={`Amount${
+                      isOptionalRequired(
+                        getValues(`ingredients[${i}].amount`),
+                        getValues(`ingredients[${i}].unit`)
+                      )
+                        ? "*"
+                        : ""
+                    }`}
+                    variant="outlined"
+                    size="medium"
+                    fullWidth
+                    helperText={renderError(errors?.ingredients?.[i]?.amount, {
+                      amount: getValues(`ingredients[${i}].amount`),
+                    })}
+                    error={
+                      !!renderError(errors?.ingredients?.[i]?.amount, {
+                        amount: getValues(`ingredients[${i}].amount`),
+                      })
+                    }
+                    {...field}
+                    onChange={(e) =>
+                      setValue(`ingredients[${i}].amount`, e.target.value, {
+                        shouldValidate: true,
+                      })
+                    }
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Controller
+                name={`ingredients[${i}].unit`}
+                control={control}
+                defaultValue={unit}
+                rules={{
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                }}
+                render={({ field }) => (
+                  <TextField
+                    label={`Unit${
+                      (getValues(`ingredients[${i}].amount`),
+                      getValues(`ingredients[${i}].unit`) ? "*" : "")
+                    }`}
+                    variant="outlined"
+                    size="medium"
+                    fullWidth
+                    helperText={renderError(errors?.ingredients?.[i]?.unit, {
+                      unit: getValues(`ingredients[${i}].unit`),
+                    })}
+                    error={
+                      !!renderError(errors?.ingredients?.[i]?.unit, {
+                        unit: getValues(`ingredients[${i}].unit`),
+                      })
+                    }
+                    {...field}
+                    onChange={(e) =>
+                      setValue(`ingredients[${i}].unit`, e.target.value, {
+                        shouldValidate: true,
+                      })
+                    }
+                  />
+                )}
+              />
+
+              {errors &&
+                errors.ingredients &&
+                errors?.ingredients[i]?.unit?.message && (
+                  <p>
+                    {errors?.ingredients[i]?.unit?.type === "pattern" &&
+                      "unit is required"}
+                  </p>
+                )}
+            </Grid>
+          </Grid>
+        );
+      })}
+      <Grid item xs={12}>
+        <Typography
+          component="span"
+          className={classes.addItem}
+          // variant={variant}
+          color="primary"
+          onClick={() => append({})}
+        >
+          Add +
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <i>
+          <Typography variant="subtitle2">
+            Fields that are marked with * sign are required.
+          </Typography>
+        </i>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button
+          size="large"
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          //@ts-expect-error
+          type="submit"
+        >
+          Checkout
+        </Button>
+      </Grid>
+    </form>
+  );
 };
 export default CriteriaForm;
+
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    flexShrink: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // minHeight: `calc(100vh - ${theme.mixins.toolbar["@media (min-width:600px)"].minHeight}px)`,
+    maxWidth: 500,
+    margin: `0 auto`,
+  },
+  section: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginTop: "50px",
+    // width: "100%",
+    // height: "100%",
+  },
+  addItem: {
+    fontWeight: "bold",
+    cursor: "pointer",
+    alignSelf: "flex-start",
+  },
+  form: {
+    width: "100%",
+  },
+  removeItem: {
+    fontWeight: "bold",
+    cursor: "pointer",
+    alignSelf: "flex-end",
+    // alignContent: "flex-end",
+    padding: "5px",
+    paddingBottom: "25px",
+  },
+}));
--- src/views/IndexView/IndexView.tsx
+++ src/views/IndexView/IndexView.tsx
@@ -1,14 +1,43 @@
 import Link from "next/link";
+
+import { Button, Typography, Grid, TextField, Box } from "@material-ui/core";
+import { makeStyles, mergeClasses } from "@material-ui/styles";
+
 import { CriteriaForm } from "components/organisms";
 
 const IndexView = () => {
+  const classes = useStyles();
+
+  const onSubmit = () => {
+    console.log("submitting!");
+  };
+
   return (
-    <div>
-      Index View
+    <div className={classes.root}>
+      {/* Index View */}
       <br />
-      <CriteriaForm />
+      <Box className={classes.formContainer}>
+        <CriteriaForm onSubmit={onSubmit} />
+      </Box>
     </div>
   );
 };
 
 export default IndexView;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    width: "100vw",
+    height: "100vh",
+    // flexDirection: "column",
+    justifyContent: "center",
+  },
+  formContainer: {
+    display: "flex",
+    maxWidth: "65vw",
+    width: "100%",
+    justifyContent: "center",
+    margin: "25px",
+  },
+}));
